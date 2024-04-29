f9466128e420a8f80b07fd1bbeb2869f8eb2c027
brian-ambani <brainambani1@gmail.com>
2024-04-29 08:17:01 +0300

    add a basic error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,30 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import BlogModel from "./models/blog";
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const blogs = await BlogModel.find().exec();
-  res.status(200).json(blogs);
+app.get("/", async (req, res, next) => {
+  try {
+    // throw new Error("This is an error");
+    const blogs = await BlogModel.find().exec();
+    res.status(200).json(blogs);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  next(Error("End point not found"));
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "Internal Server Error";
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  }
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
